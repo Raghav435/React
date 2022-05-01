a8a06cfb5b0a3ea0475fc659276cc0c255bc3837
Raghav435 <shuklaraghav435@gmail.com>
2022-05-01 23:30:59 +0530

    Road to react1

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+function navbar(){
+    return`
+    <h3>
+    <a href="./index.html">Home</a>
+</h3>
+<!-- <div class="section"> -->
+    <input type="text" id="search_receipe" placeholder="Search Receipe">
+    <h3>
+        <a href="./receipe_of_the_day.html">Get Receipe of the day</a>
+    </h3>
+    <h3>
+        <a href="./randomreceipe.html">Random Receipe</a>
+    </h3>
+    `;
+
+}
+export { navbar };
